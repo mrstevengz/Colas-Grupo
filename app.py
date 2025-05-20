6a5d3095c46847baa6d44140ec4d3fc50a85fa35
mrstevengz <ssequeira@uamv.edu.ni>
2025-05-20 14:52:19 -0600

    Codigo actualizado con funcion de colas

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import speech_recognition as sr
 import os
+from classes.classes import Cola
 
 app = Flask(__name__)
 
@@ -29,6 +30,26 @@ def transcribe():
 
     os.remove(temp_path)
     return jsonify({'text': text})
+
+@app.route('/cola', methods=['GET'])
+def obtener_cola():
+    return jsonify({'cola': Cola.items[::-1]})  
+
+@app.route('/cola', methods=['POST'])
+def encolar():
+    data = request.get_json()
+    item = data.get('item')
+    if item:
+        Cola.encolar(item)
+        return jsonify({'success': True})
+    return jsonify({'success': False, 'error': 'No item provided'}), 400
+
+@app.route('/cola', methods=['DELETE'])
+def desencolar():
+    if not Cola.is_empty():
+        desencolado = Cola.desencolar()
+        return jsonify({'success': True, 'item': desencolado})
+    return jsonify({'success': False, 'error': 'Cola vacía'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
